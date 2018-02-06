0b155e77c3857a0b34c94b5b7221420d296885ee
taliroitman <troitman.jew@gmail.com>
2018-02-06 18:13:02 +0200

    created first project FFOpen

---
+++ FirstSeleniumTest/src/test/java/FFOpen.java
@@ -0,0 +1,49 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class FFOpen  {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void testOpenFFaqq () {
+        wd.get("https://www.google.co.il/?gfe_rd=cr&dcr=0&ei=1MV5WpObCLDL8geas4GgAw");
+        wd.findElement(By.id("lst-ib")).click();
+        wd.findElement(By.id("lst-ib")).clear();
+        wd.findElement(By.id("lst-ib")).sendKeys("selenium web");
+        wd.findElement(By.cssSelector("b")).click();
+        wd.findElement(By.linkText("תמונות")).click();
+        wd.findElement(By.linkText("סרטונים")).click();
+        wd.findElement(By.xpath("//div[@id='hdtb-msb-vis']//a[.='חדשות']")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
